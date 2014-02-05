e663fc438f4af1624a08fbf0d9b570c967061be7
SeraphiPod <seraphipod@gmail.com>
2014-02-05 21:29:32 +0800

    Updated Thread.show template

--- app/views/thread/show.blade.php
+++ app/views/thread/show.blade.php
@@ -1,4 +1,23 @@
+@extends('template.master')
+
 @section('title') View Thread - {{ $thread->title }} @stop
 
 @section('content')
+	<div class="row">
+		{{-- Main Content--}}
+		<div class="col-md-9">
+			{{-- Thread --}}
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<h3> {{ $thread->title }} </h3>
+					<hr>
+					{{ $thread->body }}
+				</div>
+			</div>
+		</div>
+
+		{{-- Sidebar --}}
+		<div class="col-md-3">
+		</div>
+	</div>
 @stop
