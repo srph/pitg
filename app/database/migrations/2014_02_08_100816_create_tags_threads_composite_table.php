9b4dab49fd8b8adcc27ac9e3fa465237b9256056
JC Ricaro <jeycir@gmail.com>
2014-02-08 18:10:52 +0800

    Tags migration

---
+++ app/database/migrations/2014_02_08_100816_create_tags_threads_composite_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsThreadsCompositeTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tag_thread', function($table) {
+			$table->increments('id');
+			$table->integer('tag_id');
+			$table->integer('thread_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tag_thread');
+	}
+
+}
